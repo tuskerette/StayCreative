36e9a473d7ec3821a5f368285db559bebf8bee84
tuskerette <valeria.graffeo@gmail.com>
2015-07-13 19:52:22 -0400

    added search filter to find words in  all posts

--- public/app/app.js
+++ public/app/app.js
@@ -4,20 +4,20 @@
   app.config(function($routeProvider){
     $routeProvider
       .when('/', {
-              controller: 'myPostsController as myPostsCtrl',
-              templateUrl: 'app/views/myposts.html'
-            }).when('/list', {
-              controller: 'myPostsController as myPostsCtrl',
-              templateUrl: 'app/views/myposts.html'
-            }).when('/newpost', {
-              controller: 'myPostsController as myPostsCtrl',
-              templateUrl: 'app/views/newpost.html'
-            }).when('/myposts/:id', {
-              controller: 'myPostsController as myPostsCtrl',
-              templateUrl: 'app/views/mypost.html'
-            }).when('/edit/:id', {
-              controller: 'myPostsController as myPostsCtrl',
-              templateUrl: 'app/views/editpost.html'
+        controller: 'myPostsController as myPostsCtrl',
+        templateUrl: 'app/views/welcome.html'
+      }).when('/list', {
+        controller: 'myPostsController as myPostsCtrl',
+        templateUrl: 'app/views/myposts.html'
+      }).when('/newpost', {
+        controller: 'myPostsController as myPostsCtrl',
+        templateUrl: 'app/views/newpost.html'
+      }).when('/myposts/:id', {
+        controller: 'myPostsController as myPostsCtrl',
+        templateUrl: 'app/views/mypost.html'
+      }).when('/edit/:id', {
+        controller: 'myPostsController as myPostsCtrl',
+        templateUrl: 'app/views/editpost.html'
       }).otherwise({redirectTo: '/'});
     });
 })();
